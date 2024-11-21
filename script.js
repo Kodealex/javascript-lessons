77c5dc3172bfd1f15fca75565ca2b64ee04db56f
Kodealex <kodeloftet_alexander@hotmail.com>
2024-11-21 10:41:33 +0100

    oppdatering

---
+++ script.js
@@ -0,0 +1,19 @@
+// Green Square
+const squareGreenHide = document.querySelector("#squareGreenHide");
+const squareGreenButton = document.querySelector("#squareGreenButton");
+
+console.log(squareGreenHide, squareGreenButton);
+
+squareGreenButton.addEventListener("click", function() { 
+
+    console.log("Knapp trykket på");
+});
+
+// Red Square
+const squareRedShowText = document.querySelector("#squareRedShowText");
+const squareRedButton = document.querySelector("#squareRedButton");
+// Blue Square
+const squareBlueShowImg = document.querySelector("#squareBlueShowImg");
+const squareBlueButton = document.querySelector("#squareBlueButton");
+
+console.log(squareGreenHide, squareGreenButton, squareBlueButton, squareBlueShowImg, squareRedButton, squareRedShowText);
