8f5884859a54c7da6172d598562fae943c22eccc
Kodealex <kodeloftet_alexander@hotmail.com>
2024-11-21 11:54:40 +0100

    oppdatering

--- script.js
+++ script.js
@@ -5,15 +5,32 @@ const squareGreenButton = document.querySelector("#squareGreenButton");
 console.log(squareGreenHide, squareGreenButton);
 
 squareGreenButton.addEventListener("click", function() { 
-
+    console.log(squareGreenHide);
     console.log("Knapp trykket på");
+    squareGreenHide.classList.toggle("hidden");
 });
 
 // Red Square
 const squareRedShowText = document.querySelector("#squareRedShowText");
 const squareRedButton = document.querySelector("#squareRedButton");
+
+console.log(squareRedShowText, squareRedButton);
+
+const squareRedText = document.createElement("p");
+console.log(squareRedText);
+squareRedText.textContent = "Jeg er tekst i en rød boks, som vises når knappen trykkes på.";
+squareRedShowText.appendChild(squareRedText); 
+
+squareRedButton.addEventListener("click", function() {
+    
+    console.log("Rød knapp trykket på");
+
+    squareRedText.classList.toggle("hidden");   
+
+    
+});
+
 // Blue Square
 const squareBlueShowImg = document.querySelector("#squareBlueShowImg");
 const squareBlueButton = document.querySelector("#squareBlueButton");
 
-console.log(squareGreenHide, squareGreenButton, squareBlueButton, squareBlueShowImg, squareRedButton, squareRedShowText);
